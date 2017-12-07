b53e0fff0b64e81adee40f123bf8878fa4213498
Chetanaadhikari <31009266+Chetanaadhikari@users.noreply.github.com>
2017-12-06 21:54:22 -0500

    Add files via upload

--- Labs/GraphDemo 1/GraphDemo/MainPage.xaml.cs
+++ Labs/GraphDemo 1/GraphDemo/MainPage.xaml.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using System.Threading;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,47 +28,49 @@ namespace GraphDemo
         private byte[] data;
 
         private byte redValue, greenValue, blueValue;
+        private CancellationTokenSource tokenSource = null;
 
         public MainPage()
         {
             this.InitializeComponent();
 
-            // Initialize the array that will hold the graph data
             int dataSize = bytesPerPixel * pixelWidth * pixelHeight;
             data = new byte[dataSize];
 
-            // Initialize the bitmap that will be used to display the graph data
             graphBitmap = new WriteableBitmap(pixelWidth, pixelHeight);
         }
 
-        // Generate the data for the graph and display it
-        private void plotButton_Click(object sender, RoutedEventArgs e)
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
         {
-            // Generate random values for the RGB intensity of the graph
-            // The graph will appear in a different color each time the user clicks the Plot button
+            if (tokenSource != null)
+            {
+                tokenSource.Cancel();
+            }
+        }
+
+        private async void plotButton_Click(object sender, RoutedEventArgs e)
+        {
             Random rand = new Random();
             redValue = (byte)rand.Next(0xFF);
             greenValue = (byte)rand.Next(0xFF);
             blueValue = (byte)rand.Next(0xFF);
 
-            // Start a timer
+            tokenSource = new CancellationTokenSource();
+            CancellationToken token = tokenSource.Token;
+
             Stopwatch watch = Stopwatch.StartNew();
 
-            // Generate the data for the graph
-            // generateGraphData(data);
-            Task first = Task.Run(() => generateGraphData(data, 0, pixelWidth / 4));
-            Task second = Task.Run(() => generateGraphData(data, pixelWidth / 4, pixelWidth / 2));
-            Task.WaitAll(first, second);
-            //Task first = Task.Run(() => generateGraphData(data, 0, pixelWidth / 8));
-            //Task second = Task.Run(() => generateGraphData(data, pixelWidth / 8, pixelWidth / 4));
-            //Task third = Task.Run(() => generateGraphData(data, pixelWidth / 4, pixelWidth * 3 / 8));
-            //Task fourth = Task.Run(() => generateGraphData(data, pixelWidth * 3 / 8, pixelWidth / 2));
-            //Task.WaitAll(first, second, third, fourth);
+            try
+            {
+                await generateGraphDataAsync(data, 0, pixelWidth / 2, token);
+                duration.Text = $"Duration (ms): {watch.ElapsedMilliseconds}";
+            }
 
-            // Display the time taken to generate the data
-            duration.Text = $"Duration (ms): {watch.ElapsedMilliseconds}";
+            catch (OperationCanceledException oce)
+            {
+                duration.Text = oce.Message;
+            }
 
-            // Display the data by using the bitmap
             Stream pixelStream = graphBitmap.PixelBuffer.AsStream();
             pixelStream.Seek(0, SeekOrigin.Begin);
             pixelStream.Write(data, 0, data.Length);
@@ -73,8 +78,13 @@ namespace GraphDemo
             graphImage.Source = graphBitmap;
         }
 
-        // Complex function that generates the data for the graph
-        private void generateGraphData(byte[] data, int partitionStart, int partitionEnd)
+        private async Task generateGraphDataAsync(byte[] data, int partitionStart, int partitionEnd, CancellationToken token)
+        {
+            Task task = Task.Run(() => generateGraphData(data, partitionStart, partitionEnd, token));
+            await task;
+        }
+
+        private void generateGraphData(byte[] data, int partitionStart, int partitionEnd, CancellationToken token)
         {
             int a = pixelWidth / 2;
             int b = a * a;
@@ -86,6 +96,8 @@ namespace GraphDemo
                 double p = Math.Sqrt(b - s);
                 for (double i = -p; i < p; i += 3)
                 {
+                    token.ThrowIfCancellationRequested();
+
                     double r = Math.Sqrt(s + i * i) / a;
                     double q = (r - 1) * Math.Sin(24 * r);
                     double y = i / 3 + (q * c);
@@ -95,7 +107,6 @@ namespace GraphDemo
             }
         }
 
-        // Store the data for a given point in the graph in the array
         private void plotXY(byte[] data, int x, int y)
         {
             int pixelIndex = (x + y * pixelWidth) * bytesPerPixel;
